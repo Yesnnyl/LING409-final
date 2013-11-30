fcc539fbea96c0f2f548627dcbf88fcfc88ebf40
essemmbee <smbrown@smith.edu>
2013-11-30 10:40:44 -0500

    Update final_script.py

--- final_script.py
+++ final_script.py
@@ -1,1 +1,20 @@
 import nltk
+from nltk import parse_cfg, ChartParser
+from random import choice
+
+def produce(finalgrammar, symbol):
+    words = []
+    productions = finalgrammar.productions(lhs = symbol)
+    production = choice(productions)
+    for sym in production.rhs():
+        if isinstance(sym, str):
+            words.append(sym)
+        else:
+            words.extend(produce(finalgrammar, sym))
+    return words
+
+finalgrammar = nltk.data.load('file:grammarfinal.cfg',cache=False)
+
+parser = ChartParser(finalgrammar)
+gr = parser.finalgrammar()
+print ' '.join(produce(gr,gr.start()))
